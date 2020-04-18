1f42ed066809ada729d6473b140345536730cfc4
BenjaminCoupry <56168210+benjamincoupry@users.noreply.github.com>
2020-04-18 18:04:01 +0200

    +Cycle jour nuit ajouté dans meteo

--- core/src/Environnement/Meteo/Meteo.java
+++ core/src/Environnement/Meteo/Meteo.java
@@ -3,5 +3,19 @@ package Environnement.Meteo;
 public class Meteo {
     TemperatureMap temp;
     TemperatureMap humidite;
-    double luminosite;
+    double DureeJour;
+    double DureeNuit;
+
+    public double getLuminosite(double temps) {
+        //Part du cycle jour nuit parcouru
+        double tempsCycle = (temps % (DureeJour + DureeNuit));
+        if (tempsCycle < DureeJour) {
+            //Jour
+            double k = tempsCycle / DureeJour;
+            return (1.0 - Math.cos(Math.PI * 2.0 * k)) / 2.0;
+        } else {
+            //nuit
+            return 0;
+        }
+    }
 }
