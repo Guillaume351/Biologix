ea7f4346e924190ac51b989ff6f8d4c661a65b1c
axelle <axelle.pagnier@gmail.com>
2020-04-20 21:08:11 +0200

    petites fonctions pour les végétaux

--- core/src/Entites/Ressources/Vegetal.java
+++ core/src/Entites/Ressources/Vegetal.java
@@ -1,6 +1,10 @@
 package Entites.Ressources;
 
 public class Vegetal extends Ressource {
+
+    double energieMaxStockable; // l'énergie maximale stockable par la plante
+    double energieVegetal; //l'énergie propre à la plante, dont elle a besoin pour grandir etc. energieVegetal = 0 <=> plante = dead. energieVegetal = 1 <=> plante en pleine forme
+
     @Override
     public double getQuantiteEnergie() {
         return super.getQuantiteEnergie();
@@ -13,13 +17,28 @@ public class Vegetal extends Ressource {
 
     @Override
     public void setQuantiteEnergie(double quantiteEnergie) {
-        this.quantiteEnergie = quantiteEnergie;
+        if (quantiteEnergie < this.energieMaxStockable) {
+            this.quantiteEnergie = quantiteEnergie;
+        } else { this.quantiteEnergie = this.energieMaxStockable; }
     }
 
     /**
-     * créer un végétal
+     * définir l'énergie maximale stockable par la plante
+     * @param energieMax
      */
-    public void creerVégétal() {
-        setQuantiteEnergie(0);
+    public void setEnergieMaxStockable(double energieMax) {
+        this.energieMaxStockable = energieMax;
+    }
+
+    /** Ce qu'il se passe lorsque la pauvre plante se fait bouffer
+     *
+     * @param quantiteIngeree, la quantité d'énergie ingérée par une créature
+     */
+    public void estMange(double quantiteIngeree) {
+        if (getQuantiteEnergie() >= quantiteIngeree) {
+            setQuantiteEnergie(getQuantiteEnergie() - quantiteIngeree);
+        } else {
+            setQuantiteEnergie(0);
+        }
     }
 }
