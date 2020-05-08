ff92f6f910f7ff3587363d0ca2d9292e06b056e4
BenjaminCoupry <56168210+BenjaminCoupry@users.noreply.github.com>
2020-05-08 17:12:54 +0200

    +Structure metoMap

--- core/src/Environnement/Meteo/MeteoMap.java
+++ core/src/Environnement/Meteo/MeteoMap.java
@@ -8,16 +8,22 @@ public class MeteoMap {
     HeightMap moyennes;
     PerlinParams paramsTemporel;
     double variabilite;
+    double temps;
 
     public MeteoMap(PerlinParams params, double min, double max) {
         //TODO : faire la Heightmap des moyennes
+        temps = 0;
     }
 
     public double getTemp(double x, double y) {
-        return getTemp(x, y, 0);//TODO : REMPLACER PAR LE TEMPS !
+        return getTemp(x, y, temps);//TODO : REMPLACER PAR LE TEMPS !
     }
 
     public double getTemp(double x, double y, double t) {
         return moyennes.getValeur(x, y) + variabilite * PerlinGenerator.perlin1D(t, paramsTemporel);
+    }
+
+    public void update(double dt) {
+        temps += dt;
     }
 }
