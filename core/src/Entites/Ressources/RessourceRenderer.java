072d217010b58405e83653f8d462f75d37d0f540
axelle <axelle.pagnier@gmail.com>
2020-05-10 15:36:39 +0200

    ajout d'images (elles sont moches mdr mais c'est déjà ça) +début de RessourceRenderer

---
+++ core/src/Entites/Ressources/RessourceRenderer.java
@@ -0,0 +1,12 @@
+package Entites.Ressources;
+
+import java.util.ArrayList;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class RessourceRenderer {
+
+    public ArrayList[] tableauRessources = new ArrayList[3];  //3 types de resources différents
+}
