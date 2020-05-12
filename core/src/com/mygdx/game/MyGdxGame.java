9b93585055f45b6104e2c0ea31901bc8c0321b66
corentin <corentin.dmz@live.fr>
2020-05-12 19:48:37 +0200

    début écran démarrage

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -6,6 +6,7 @@ import Entites.Ressources.RessourceRenderer;
 import Environnement.Terrain.Terrain;
 import Environnement.Terrain.TerrainGenerator;
 import Environnement.Terrain.TerrainRenderer;
+import Utils.EcranDemarrage;
 import Utils.EcranSimulation;
 import Utils.Perlin.PerlinParams;
 import com.badlogic.gdx.ApplicationAdapter;
@@ -18,6 +19,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.MapRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -28,11 +30,14 @@ import static com.badlogic.gdx.Gdx.input;
 
 public class MyGdxGame extends Game {
 
-    EcranSimulation ecranSimulation;
+    EcranDemarrage ecranDemarrage;
+    public EcranSimulation ecranSimulation;
 
     @Override
     public void create() {
+        InputEvent event = new InputEvent();
+        this.ecranDemarrage = new EcranDemarrage(this);
         this.ecranSimulation = new EcranSimulation();
-        setScreen(ecranSimulation);
+        setScreen(ecranDemarrage);
     }
 }
